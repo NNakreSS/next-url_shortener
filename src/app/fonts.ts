ad6f200b8c867c82400352eadfe8f14063563b69
NNakreSS <serkanatmaca15@gmail.com>
2024-04-20 00:18:47 +0300

    create hero component , add kanit fontWeights

--- src/app/fonts.ts
+++ src/app/fonts.ts
@@ -2,7 +2,7 @@ import { Inter, Kanit } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 const kanit = Kanit({
   subsets: ["latin"],
-  weight: ["900", "800", "700", "600", "500", "400", "300"],
+  weight: ["900", "800", "700", "600", "500", "400", "300", "200", "100"],
 });
 
 export { inter, kanit };
