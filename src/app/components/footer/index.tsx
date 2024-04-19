863ff08eb03a3f963edd0b54a5d4d166aaa45169
NNakreSS <serkanatmaca15@gmail.com>
2024-04-19 22:03:36 +0300

    Create Footer

--- src/app/components/footer/index.tsx
+++ src/app/components/footer/index.tsx
@@ -1,7 +1,27 @@
+import { kanit } from "@/app/fonts";
+import clsx from "clsx";
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer
+      className={clsx(
+        kanit.className,
+        "flex col-span-full justify-center items-center px-1 md:px-10 lg:px-40 py-3 border-t-2 border-dark-green"
+      )}
+    >
+      <div className="flex flex-col justify-center items-center font-thin">
+        <span>&copy; 2024 nkLink Tüm hakları saklıdır</span>
+        <p>
+          nkLink {" "}
+          <span className="font-medium text-dark-green">
+            NakreS Development
+          </span>{" "}
+          tarafından geliştirilmiştir.
+        </p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
