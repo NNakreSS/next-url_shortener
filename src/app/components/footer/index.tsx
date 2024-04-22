2bd61d31c504a67a727c4370dc501de2221b8a9e
NNakreSS <serkanatmaca15@gmail.com>
2024-04-22 20:12:13 +0300

    set height

--- src/app/components/footer/index.tsx
+++ src/app/components/footer/index.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
     <footer
       className={clsx(
         kanit.className,
-        "flex col-span-full justify-center items-center px-1 md:px-10 lg:px-40 py-3 border-t-2 border-dark-green"
+        "h-20 flex col-span-full justify-center items-center border-t-2 border-dark-green"
       )}
     >
       <div className="flex flex-col justify-center items-center font-thin">
