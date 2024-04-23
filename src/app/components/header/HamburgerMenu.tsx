26b49269eb533c3a5af58d4993514d87f534e82b
NNakreSS <serkanatmaca15@gmail.com>
2024-04-23 10:46:05 +0300

    Replace BurgerMenu Icon

--- src/app/components/header/HamburgerMenu.tsx
+++ src/app/components/header/HamburgerMenu.tsx
@@ -1,9 +1,9 @@
-import { Bars3Icon } from "@heroicons/react/24/solid";
+import { RxHamburgerMenu } from "react-icons/rx";
 
 const HamburgerMenu = () => {
   return (
     <div className="md:hidden">
-      <Bars3Icon className="w-10 h-10 text-dark-green" />
+      <RxHamburgerMenu className="text-5xl text-dark-green" />
     </div>
   );
 };
