cc8b5c72dc9d609705dfefa96a2091bb314bac43
NNakreSS <serkanatmaca15@gmail.com>
2024-04-23 10:37:26 +0300

    Create Services

--- src/app/components/about/About.tsx
+++ src/app/components/about/About.tsx
@@ -1,9 +1,11 @@
+import Services from "./Services";
 import WhoWeAre from "./WhoWeAre";
 
 function About() {
   return (
-    <section className="py-12">
+    <section className="py-12 flex flex-wrap gap-20">
       <WhoWeAre />
+      <Services />
     </section>
   );
 }
---
+++ src/app/components/about/Services.tsx
@@ -0,0 +1,48 @@
+// icons
+import { AiOutlineApi } from "react-icons/ai";
+import { LuLayoutPanelLeft } from "react-icons/lu";
+import { BiMoneyWithdraw } from "react-icons/bi";
+import { SiPagespeedinsights } from "react-icons/si";
+// fonts
+import { kanit } from "@/app/fonts";
+//
+import clsx from "clsx";
+
+const ServicesData = [
+  {
+    title: "API Hizmeti",
+    icon: AiOutlineApi,
+    text: "Bir API key  edinerek  link kısaltma hizmetini kendi projelerinizde kullanabilirsiniz",
+  },
+  {
+    title: "Yönetim Paneli",
+    icon: LuLayoutPanelLeft,
+    text: "Yönetim paneli kolaylığı sayesinde linklerinizi düzenli şekilde takip edin , detaylı tıklama takibi sayesinde içeriklerinizi yönetin",
+  },
+  {
+    title: "Ücretsiz Hizmet",
+    icon: BiMoneyWithdraw,
+    text: "Tüm hizmetlerden ücretsiz bir şekilde yararlanabilirsiniz    ",
+  },
+  {
+    title: "Hız",
+    icon: SiPagespeedinsights,
+    text: "Hızlı hizmet sayesinde gereksiz zaman kaybından kurtulun",
+  },
+];
+
+function Services() {
+  return (
+    <div className={clsx(kanit.className, "grid grid-cols-4 gap-2 text-white")}>
+      {ServicesData.map(({ title, text, icon: Icon }) => (
+        <div className="flex flex-col gap-5 p-5 bg-Blue rounded-md">
+          <Icon className="text-5xl" />
+          <h4 className="text-3xl font-medium">{title}</h4>
+          <p className="text-xl font-light text-off-wite">{text}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Services;
