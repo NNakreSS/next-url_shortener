5ccc328022fe591cd08638cb867c0c144e4b1546
NNakreSS <serkanatmaca15@gmail.com>
2024-04-24 18:24:04 +0300

    Create SignYp page

--- src/app/(pages)/(loginpages)/signup/page.tsx
+++ src/app/(pages)/(loginpages)/signup/page.tsx
@@ -1,7 +1,34 @@
+import TextInput from "@/app/components/TextInput";
+import Link from "next/link";
+// icons
+import { IoIosLock } from "react-icons/io";
+
 export default function SignUp() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Sign Up</h1>
-    </main>
+    <div className="bg-Blue flex flex-col gap-5 mx-auto px-5 py-10 rounded-md w-full md:w-4/5 lg:w-2/5 my-5">
+      <h1 className="text-white text-4xl font-medium">Hesap Oluştur</h1>
+      <form action="" method="post" className="flex flex-col gap-2">
+        <TextInput placeholder="Kullanıcı Adı " />
+        <TextInput placeholder="E-posta" />
+        <TextInput placeholder="Şifre" />
+        <TextInput placeholder="Şifre (Tekrar)" />
+        <button
+          type="submit"
+          className="bg-green text-white rounded-md p-2 text-2xl font-medium mt-2"
+        >
+          Kayıt Ol
+        </button>
+      </form>
+
+      <div className="flex flex-col gap-2">
+        <h2 className="text-white text-2xl font-medium">Zaten üye misin ?</h2>
+        <Link
+          href="/signin"
+          className="bg-white w-full rounded-md p-2 text-lg font-medium text-center"
+        >
+          Giriş Yap
+        </Link>
+      </div>
+    </div>
   );
 }
