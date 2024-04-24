162f720acc39accf7c5dc06134fbec6ad3ae812d
NNakreSS <serkanatmaca15@gmail.com>
2024-04-25 01:47:21 +0300

    use MainContainer

--- src/app/(pages)/(loginpages)/layout.tsx
+++ src/app/(pages)/(loginpages)/layout.tsx
@@ -1,16 +1,18 @@
 // styles
+import "@/app/styles/loginpage.css";
+// components
+import MainContainer from "@/app/components/MainContainer";
 import Footer from "@/app/components/footer";
 import Header from "@/app/components/header";
-import "@/app/styles/loginpage.css";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="container flex flex-col md:gap-x-5 m-auto min-h-screen">
       <Header />
-      <main className="px-1 lg:px-40 grow relative h-full w-full">
+      <MainContainer>
         <div className="absolute top-0 right-0 w-full bg-light-aqua h-56 login-page-background -z-50" />
         {children}
-      </main>
+      </MainContainer>
       <Footer />
     </div>
   );
