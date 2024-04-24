f8ae3313376c7ffe32f7bfcebd8cbea44bb97472
NNakreSS <serkanatmaca15@gmail.com>
2024-04-24 18:24:19 +0300

    initial commit

---
+++ src/app/components/TextInput.tsx
@@ -0,0 +1,22 @@
+function TextInput({
+  placeholder,
+  ...props
+}: {
+  placeholder: string;
+  props?: any;
+}) {
+  return (
+    <label className="w-full flex flex-col bg-white relative rounded-md">
+      <input
+        {...props}
+        className="w-full h-full mt-5 bg-transparent rounded-md p-2 border-none outline-none transition duration-200 font-light text-lg"
+        type="text"
+      />
+      <span className="h-full content-center absolute left-0 top-0 px-2 text-lg font-extralight transition duration-200 input-label">
+        {placeholder}
+      </span>
+    </label>
+  );
+}
+
+export default TextInput;
