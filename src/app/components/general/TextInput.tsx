808e61bb7cf6b2eb9cd3b0d690cb3efc5f418cf2
NNakreSS <serkanatmaca15@gmail.com>
2024-04-29 18:35:50 +0300

    fix style for erros border

--- src/app/components/general/TextInput.tsx
+++ src/app/components/general/TextInput.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { clsx } from "clsx";
+import { error } from "console";
 import { FieldErrors, FieldValues, UseFormRegister } from "react-hook-form";
 
 interface InputProps {
@@ -21,16 +22,19 @@ function TextInput({
   disabled,
   ...props
 }: InputProps) {
+  console.log(errors);
   return (
-    <label className="w-full flex flex-col bg-white relative rounded-md">
+    <label
+      className={clsx(
+        "w-full flex flex-col bg-white relative rounded-md",
+        errors[id] && "border-2 border-red-500"
+      )}
+    >
       <input
         disabled={disabled}
         {...register(id, { required })}
         {...props}
-        className={clsx(
-          "w-full h-full mt-5 bg-transparent rounded-md p-2 border-none outline-none transition duration-200 font-light text-lg",
-          errors[id] && "border border-red-500"
-        )}
+        className="w-full h-full mt-5 bg-transparent rounded-md p-2 border-none outline-none transition duration-200 font-light text-lg"
         type="text"
       />
       <span className="h-full content-center absolute left-0 top-0 px-2 text-lg font-extralight transition duration-200 input-label">
