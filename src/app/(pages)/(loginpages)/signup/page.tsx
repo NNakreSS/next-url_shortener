2aa2ff0dc6e6179655403d1fc0c41fdc7a73e843
NNakreSS <serkanatmaca15@gmail.com>
2024-04-29 18:34:55 +0300

    use client for forms

--- src/app/(pages)/(loginpages)/signup/page.tsx
+++ src/app/(pages)/(loginpages)/signup/page.tsx
@@ -1,32 +1,6 @@
-import TextInput from "@/app/components/general/TextInput";
-import Link from "next/link";
+// component
+import RegisterClient from "@/app/components/auth/RegisterClient";
 
 export default function SignUp() {
-  return (
-    <div className="bg-Blue flex flex-col gap-5 mx-auto px-5 py-10 rounded-md w-full md:w-4/5 lg:w-2/5 my-5">
-      <h1 className="text-white text-4xl font-medium">Hesap Oluştur</h1>
-      <form action="" method="post" className="flex flex-col gap-2">
-        <TextInput placeholder="Kullanıcı Adı " />
-        <TextInput placeholder="E-posta" />
-        <TextInput placeholder="Şifre" />
-        <TextInput placeholder="Şifre (Tekrar)" />
-        <button
-          type="submit"
-          className="bg-green text-white rounded-md p-2 text-2xl font-medium mt-2"
-        >
-          Kayıt Ol
-        </button>
-      </form>
-
-      <div className="flex flex-col gap-2">
-        <h2 className="text-white text-2xl font-medium">Zaten üye misin ?</h2>
-        <Link
-          href="/signin"
-          className="bg-white w-full rounded-md p-2 text-lg font-medium text-center"
-        >
-          Giriş Yap
-        </Link>
-      </div>
-    </div>
-  );
+  return <RegisterClient />;
 }
