6358a0c7552410d760589da2e9a779f9c2c7f00b
NNakreSS <serkanatmaca15@gmail.com>
2024-04-29 18:35:10 +0300

    initial commit

---
+++ src/app/components/auth/LoginClient.tsx
@@ -0,0 +1,67 @@
+"use client";
+
+import Link from "next/link";
+// components
+import TextInput from "../general/TextInput";
+// icons
+import { IoIosLock } from "react-icons/io";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+
+function LoginClient() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<FieldValues>();
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => console.log(data);
+
+  return (
+    <>
+      <h1 className="text-white text-4xl font-medium">Giriş Yap</h1>
+      <form action="" method="post" className="flex flex-col gap-2">
+        <TextInput
+          id="e-mail"
+          register={register}
+          errors={errors}
+          placeholder="E-posta"
+          required
+        />
+        <TextInput
+          id="password"
+          register={register}
+          errors={errors}
+          placeholder="Şifre"
+          required
+        />
+        <button
+          type="submit"
+          className="bg-green text-white rounded-md p-2 text-2xl font-medium mt-2"
+          onClick={handleSubmit(onSubmit)}
+        >
+          Giriş Yap
+        </button>
+      </form>
+
+      <button className="flex items-center gap-2 w-auto my-5">
+        <IoIosLock className="text-green text-2xl" />
+        <span className="text-base font-extralight text-white">
+          Şifremi Unuttum
+        </span>
+      </button>
+
+      <div className="flex flex-col gap-2">
+        <h2 className="text-white text-2xl font-medium">Üye değil misin ?</h2>
+        <Link
+          href="/signup"
+          className="bg-white w-full rounded-md p-2 text-lg font-medium text-center"
+        >
+          Hesap Oluştur
+        </Link>
+      </div>
+    </>
+  );
+}
+
+export default LoginClient;
---
+++ src/app/components/auth/RegisterClient.tsx
@@ -0,0 +1,71 @@
+"use client";
+
+import Link from "next/link";
+// components
+import TextInput from "../general/TextInput";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+
+function RegisterClient() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<FieldValues>();
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => console.log(data);
+
+  return (
+    <>
+      <h1 className="text-white text-4xl font-medium">Hesap Oluştur</h1>
+      <form action="" method="post" className="flex flex-col gap-2">
+        <TextInput
+          id="userName"
+          register={register}
+          errors={errors}
+          required
+          placeholder="Kullanıcı Adı "
+        />
+        <TextInput
+          id="e-mail"
+          register={register}
+          errors={errors}
+          required
+          placeholder="E-posta"
+        />
+        <TextInput
+          id="password"
+          register={register}
+          errors={errors}
+          required
+          placeholder="Şifre"
+        />
+        <TextInput
+          id="passwordagain"
+          register={register}
+          errors={errors}
+          required
+          placeholder="Şifre (Tekrar)"
+        />
+        <button
+          type="submit"
+          className="bg-green text-white rounded-md p-2 text-2xl font-medium mt-2"
+        >
+          Kayıt Ol
+        </button>
+      </form>
+
+      <div className="flex flex-col gap-2">
+        <h2 className="text-white text-2xl font-medium">Zaten üye misin ?</h2>
+        <Link
+          href="/signin"
+          className="bg-white w-full rounded-md p-2 text-lg font-medium text-center"
+        >
+          Giriş Yap
+        </Link>
+      </div>
+    </>
+  );
+}
+
+export default RegisterClient;
