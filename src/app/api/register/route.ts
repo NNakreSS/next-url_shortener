6ce0d739581dd051155b405b5055d6a0cc4b001c
NNakreSS <serkanatmaca15@gmail.com>
2024-05-01 17:50:18 +0300

    clear logs

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -3,14 +3,10 @@ import prisma from "@/libs/prismadb";
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
-  console.log("sa");
   const body = await req.json();
-  console.log(body);
+  // console.log(body);
   const { userName: name, email, password } = body;
-  console.log(name, email, password);
   const hashedPassword = await bcrypt.hash(password, 10);
-  console.log(hashedPassword);
-
   const user = await prisma.user.create({
     data: {
       name,
