9cdfcc26af99954cd5340c8ca5238eb5978553ad
NNakreSS <serkanatmaca15@gmail.com>
2024-05-03 21:23:14 +0300

    add current user state

--- src/app/(pages)/auth/signup/page.tsx
+++ src/app/(pages)/auth/signup/page.tsx
@@ -1,6 +1,8 @@
 // component
+import { getCurrentUser } from "@/app/actions/getCurrentUser";
 import RegisterClient from "@/app/components/auth/RegisterClient";
 
-export default function SignUp() {
-  return <RegisterClient />;
+export default async function SignUp() {
+  const currentUser = await getCurrentUser();
+  return <RegisterClient currentUser={currentUser} />;
 }
