b4fd8dce8e1ae297f75b766c1e1418b8533a75c1
NNakreSS <serkanatmaca15@gmail.com>
2024-05-03 22:03:06 +0300

    create chain middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,48 +1,6 @@
-import { NextResponse, NextRequest } from "next/server";
-import { getToken } from "next-auth/jwt";
-import type { Role } from "@prisma/client";
-import { PATH_DEFAULT, PATH_SIGNIN, PATH_SIGNUP } from "./libs/routes";
-// custom hooks
-import { useAuth } from "./hooks/useAuth";
+import { chain } from "./middlewares/chain";
+import withAuth from "./middlewares/withAuth";
 
-interface currentUserType {
-  name: string;
-  email: string;
-  image?: string;
-  role: Role;
-}
-
-async function withAuth(req: NextRequest) {
-  const currentUser = (await getToken({
-    req: req,
-    secret: process.env.SECRET,
-  })) as currentUserType | null;
-
-  const pathname = req.nextUrl.pathname;
-  const { requireLogin, allowedRoles } = useAuth(pathname);
-
-  if (pathname == PATH_SIGNIN || pathname == PATH_SIGNUP) {
-    if (currentUser) {
-      return NextResponse.redirect(new URL(PATH_DEFAULT, req.url));
-    }
-  }
-
-  if (requireLogin) {
-    if (!currentUser) {
-      return NextResponse.redirect(new URL(PATH_SIGNIN, req.url));
-    } else {
-      if (allowedRoles && !allowedRoles.includes(currentUser?.role)) {
-        return NextResponse.redirect(new URL(PATH_SIGNIN, req.url));
-      }
-    }
-  }
-
-  return null;
-}
-
-export default async function middleware(req: NextRequest) {
-  const m1 = await withAuth(req);
-  return m1;
-}
+export default chain([withAuth]);
 
 export const config = { matcher: ["/dashboard", "/admin", "/auth/:path*"] };
