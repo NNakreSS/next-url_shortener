3c0a3c3bcf1cdf1468d7061f152140a763aab08b
NNakreSS <serkanatmaca15@gmail.com>
2024-05-03 21:26:09 +0300

    use client

--- src/app/components/header/Logo.tsx
+++ src/app/components/header/Logo.tsx
@@ -1,6 +1,14 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
 const Logo = () => {
+  const router = useRouter();
   return (
-    <div className="text-4xl text-dark-green font-extrabold h-full flex items-center justify-center">
+    <div
+      onClick={() => router.push("/")}
+      className="text-4xl text-dark-green font-extrabold h-full flex items-center justify-center cursor-pointer"
+    >
       Logo
     </div>
   );
