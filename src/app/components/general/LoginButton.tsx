b9a045c34a21330d0a817078481428230f390613
NNakreSS <serkanatmaca15@gmail.com>
2024-05-03 21:25:41 +0300

    show login info

--- src/app/components/general/LoginButton.tsx
+++ src/app/components/general/LoginButton.tsx
@@ -1,8 +1,18 @@
+"use client";
 import clsx from "clsx";
 import Link from "next/link";
+// icons
+import { AiOutlineUser } from "react-icons/ai";
+// types
+import currentUser from "@/types/currentUser";
 
-function LoginButton({ className }: { className?: string }) {
-  return (
+interface UserProps {
+  className?: string;
+  currentUser: currentUser | null | undefined;
+}
+
+function LoginButton({ className, currentUser }: UserProps) {
+  return !currentUser?.name ? (
     <Link
       href="/auth/signin"
       className={clsx(
@@ -11,6 +21,14 @@ function LoginButton({ className }: { className?: string }) {
       )}
     >
       Giriş Yap
+    </Link>
+  ) : (
+    <Link
+      href="/dashboard"
+      className="text-white bg-green py-2 px-3 rounded-md text-xl !opacity-100 flex justify-between items-center gap-2 cursor-pointer"
+    >
+      <AiOutlineUser />
+      <span> {currentUser?.name}</span>
     </Link>
   );
 }
