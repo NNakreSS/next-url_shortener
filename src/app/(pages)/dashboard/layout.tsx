fe88aaf4a50a89ecbae3898179bad20dcb50aa16
NNakreSS <serkanatmaca15@gmail.com>
2024-05-04 19:12:39 +0300

    add style

--- src/app/(pages)/dashboard/layout.tsx
+++ src/app/(pages)/dashboard/layout.tsx
@@ -1,8 +1,11 @@
+import Header from "@/app/components/dashboard/Header";
+import MainContainer from "@/app/components/general/MainContainer";
+
 export default function Layout({ children }: { children: React.ReactNode }) {
-    return (
-      <div className="h-screen w-full">
-        <div className="mx-auto">{children}</div>
-      </div>
-    );
-  }
-  
+  return (
+    <div className="container flex flex-col md:gap-x-5 m-auto min-h-screen">
+      <Header />
+      <MainContainer>{children}</MainContainer>
+    </div>
+  );
+}
