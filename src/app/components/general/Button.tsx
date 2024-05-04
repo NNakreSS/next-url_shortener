d1d9fd48edd85c8fea8f88cee835675084ff8a59
NNakreSS <serkanatmaca15@gmail.com>
2024-05-04 11:56:06 +0300

    initial commit

---
+++ src/app/components/general/Button.tsx
@@ -0,0 +1,29 @@
+import { AiOutlineLoading } from "react-icons/ai";
+
+interface ButtonProps {
+  className?: string;
+  disabled?: boolean;
+  type?: "submit" | "reset" | "button";
+  onClick?: () => void;
+  loading?: boolean;
+  label: string;
+}
+
+function Button({ className, label, loading, onClick }: ButtonProps) {
+  return (
+    <button
+      disabled={loading}
+      type="submit"
+      className={className}
+      onClick={onClick}
+    >
+      {loading ? (
+        <AiOutlineLoading className="animate-spin text-3xl" />
+      ) : (
+        <span>{label}</span>
+      )}
+    </button>
+  );
+}
+
+export default Button;
