24876eb55e590a1788f0e0c71962e1d4634a7fc6
NNakreSS <serkanatmaca15@gmail.com>
2024-05-04 19:12:29 +0300

    remove currentUser

--- src/app/(pages)/auth/signup/page.tsx
+++ src/app/(pages)/auth/signup/page.tsx
@@ -1,8 +1,6 @@
 // component
-import { getCurrentUser } from "@/app/actions/getCurrentUser";
 import RegisterClient from "@/app/components/auth/RegisterClient";
 
 export default async function SignUp() {
-  const currentUser = await getCurrentUser();
-  return <RegisterClient currentUser={currentUser} />;
+  return <RegisterClient />;
 }
