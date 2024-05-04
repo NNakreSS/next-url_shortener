0af98daf8632484546a972e676a2e0299150feea
NNakreSS <serkanatmaca15@gmail.com>
2024-05-04 19:13:05 +0300

    remove

--- src/app/(pages)/dashboard/api_keys/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function ApiKeys() {
-    return (
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <h1>API KEYS</h1>
-      </main>
-    );
-  }
-  
