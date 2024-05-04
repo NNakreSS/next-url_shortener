ef8d1a754ced5defb71e653c6d5e531cd76fad9e
NNakreSS <serkanatmaca15@gmail.com>
2024-05-04 19:12:51 +0300

    create sections

--- src/app/(pages)/dashboard/page.tsx
+++ src/app/(pages)/dashboard/page.tsx
@@ -1,7 +1,15 @@
+import LastClickCard from "@/app/components/dashboard/main/LastClickCard";
+import LinkManagment from "@/app/components/dashboard/main/LinkManagment";
+import TotalCards from "@/app/components/dashboard/main/TotalCards";
+
 export default function Dashboard() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>DASHBOARD</h1>
-    </main>
+    <>
+      <TotalCards />
+      <div className="grid grid-cols-3 gap-5 mb-5">
+        <LinkManagment className="col-span-2" />
+        <LastClickCard />
+      </div>
+    </>
   );
 }
