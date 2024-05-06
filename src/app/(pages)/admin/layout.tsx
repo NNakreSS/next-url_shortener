c9423f055e93d4c03da017751f317221b1d83e61
NNakreSS <serkanatmaca15@gmail.com>
2024-05-06 19:55:27 +0300

    move other folder

--- src/app/(pages)/admin/layout.tsx
+++
@@ -1,8 +0,0 @@
-export default function Layout({ children }: { children: React.ReactNode }) {
-    return (
-      <div className="h-screen w-full">
-        <div className="mx-auto">{children}</div>
-      </div>
-    );
-  }
-  
--- src/app/(pages)/admin/links/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Links() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Links</h1>
-    </main>
-  );
-}
