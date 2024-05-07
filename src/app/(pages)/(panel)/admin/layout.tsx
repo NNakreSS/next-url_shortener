5118a1a187b16740266c96b41b6b197c8c0c91f0
NNakreSS <serkanatmaca15@gmail.com>
2024-05-07 15:37:38 +0300

    create

--- src/app/(pages)/(panel)/admin/layout.tsx
+++ src/app/(pages)/(panel)/admin/layout.tsx
@@ -1,8 +1,22 @@
+import MainContainer from "@/app/components/general/MainContainer";
+import { Header, Menu } from "@/app/components/header";
+import User from "@/app/components/header/User";
+import { PATH_ADMIN } from "@/libs/routes";
+import Navigation from "@/types/NavigationType";
+
+const navigations: Navigation[] = [
+  { name: "Anasayfa", path: PATH_ADMIN },
+  { name: "Üyeler", path: "üyeler" },
+  { name: "Linkler", path: "links" },
+];
 export default function Layout({ children }: { children: React.ReactNode }) {
-    return (
-      <div className="h-screen w-full">
-        <div className="mx-auto">{children}</div>
-      </div>
-    );
-  }
-  
+  return (
+    <div className="container flex flex-col md:gap-x-5 m-auto min-h-screen">
+      <Header>
+        <Menu navigations={navigations} />
+        <User />
+      </Header>
+      <MainContainer>{children}</MainContainer>
+    </div>
+  );
+}
