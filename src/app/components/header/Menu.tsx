cb8d908677a983bfa41fb241202b72231b3961d8
NNakreSS <serkanatmaca15@gmail.com>
2024-05-07 15:38:57 +0300

    refactor

--- src/app/components/header/Menu.tsx
+++ src/app/components/header/Menu.tsx
@@ -4,19 +4,28 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 //styles
 import "@/app/styles/menu.css";
+import Navigation from "@/types/NavigationType";
+import HamburgerMenu from "./HamburgerMenu";
 
-function Menu() {
+function Menu({ navigations }: { navigations: Navigation[] }) {
   const pathname = usePathname();
 
   return (
-    <nav className="gap-5 text-dark-green font-normal text-2xl hidden md:flex items-center justify-center">
-      <Link className={pathname == "/" ? "active" : ""} href="/">
-        Anasayfa
-      </Link>
-      <Link className={pathname == "/contact" ? "active" : ""} href="/contact">
-        İletişim
-      </Link>
-    </nav>
+    <div>
+      <nav className="gap-5 text-dark-green font-normal text-2xl hidden md:flex items-center justify-center">
+        {navigations.map((menu: any, i: number) => (
+          <Link
+            key={i}
+            className={pathname == menu.path ? "active" : ""}
+            href={menu.path}
+          >
+            {menu.name}
+          </Link>
+        ))}
+      </nav>
+
+      <HamburgerMenu navigations={navigations} />
+    </div>
   );
 }
 
