1ef970ce9369add9ef8819184000dfe9e9334004
NNakreSS <serkanatmaca15@gmail.com>
2024-05-13 16:21:42 +0300

    fix component

--- src/app/(pages)/(panel)/admin/users/page.tsx
+++ src/app/(pages)/(panel)/admin/users/page.tsx
@@ -1,7 +1,5 @@
+import UsersTable from "@/app/components/admin/users/UsersTable";
+
 export default function Users() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Users</h1>
-    </main>
-  );
+  return <UsersTable />;
 }
