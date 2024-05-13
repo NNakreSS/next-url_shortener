29644c0df262e11d26704249382ffc81b5ed9cec
NNakreSS <serkanatmaca15@gmail.com>
2024-05-13 16:21:14 +0300

    add paths

--- src/app/(pages)/(panel)/admin/layout.tsx
+++ src/app/(pages)/(panel)/admin/layout.tsx
@@ -1,13 +1,13 @@
 import MainContainer from "@/app/components/general/MainContainer";
 import { Header, Menu } from "@/app/components/header";
 import User from "@/app/components/header/User";
-import { PATH_ADMIN } from "@/libs/routes";
+import { ADMIN_LINKS, ADMIN_USERS, PATH_ADMIN } from "@/libs/routes";
 import Navigation from "@/types/NavigationType";
 
 const navigations: Navigation[] = [
   { name: "Anasayfa", path: PATH_ADMIN },
-  { name: "Üyeler", path: "üyeler" },
-  { name: "Linkler", path: "links" },
+  { name: "Üyeler", path: ADMIN_USERS },
+  { name: "Linkler", path: ADMIN_LINKS },
 ];
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
