1f5a40148e1654c124367abd73dc5f4c72554c64
NNakreSS <serkanatmaca15@gmail.com>
2024-05-13 16:21:20 +0300

    create page

--- src/app/(pages)/(panel)/admin/page.tsx
+++ src/app/(pages)/(panel)/admin/page.tsx
@@ -1,7 +1,17 @@
+import AdminTotalCards from "@/app/components/admin/main/AdminTotalCards";
+import LastLinksCard from "@/app/components/admin/main/LastLinks";
+import LastUsersCard from "@/app/components/admin/main/LastUsers";
+import LastClickCard from "@/app/components/dashboard/main/LastClickCard";
+
 export default function Admin() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>ADMIN</h1>
-    </main>
+    <>
+      <AdminTotalCards />
+      <section className="grid grid-cols-1 lg:grid-cols-3 gap-3 mb-5">
+        <LastLinksCard />
+        <LastClickCard />
+        <LastUsersCard />
+      </section>
+    </>
   );
 }
