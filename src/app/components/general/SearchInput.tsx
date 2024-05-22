c4431d8b7ed927429ce559d5ff1d2a0aefd1b5c1
NNakreSS <serkanatmaca15@gmail.com>
2024-05-22 16:39:21 +0300

    fix search component

--- src/app/components/general/SearchInput.tsx
+++ src/app/components/general/SearchInput.tsx
@@ -1,10 +1,16 @@
 "use client";
 
+import clsx from "clsx";
 import { IoMdSearch } from "react-icons/io";
 
-function SearchInput() {
+function SearchInput({ className }: { className?: string }) {
   return (
-    <div className="shadow-cover p-2 rounded-lg flex gap-2 items-center justify-between ">
+    <div
+      className={clsx(
+        "shadow-cover p-2 rounded-lg flex gap-2 items-center justify-between h-10",
+        className
+      )}
+    >
       <IoMdSearch className="text-2xl text-green" />
       <input
         placeholder="İsme göre ara"
