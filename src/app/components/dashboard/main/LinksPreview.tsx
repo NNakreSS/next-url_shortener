747229e2f0ed3d3f40a0334ac18aed65858dc85f
NNakreSS <serkanatmaca15@gmail.com>
2024-05-23 20:49:28 +0300

    remove moch data

--- src/app/components/dashboard/main/LinksPreview.tsx
+++ src/app/components/dashboard/main/LinksPreview.tsx
@@ -1,45 +1,11 @@
-import { LuLink } from "react-icons/lu";
+"use client";
 import LinkCard from "./LinkCard";
+import LinkType from "@/types/LinkType";
 
-const shortLinkData = [
-  {
-    tag: "discord",
-    clickCount: 20,
-    originalUrl: "http://discord",
-    shortUrl: "link.nakres.dev/",
-    shortenedDate: "2024-09-16",
-    shortenedTime: "00:58:47",
-  },
-  {
-    tag: "discord",
-    clickCount: 20,
-    originalUrl: "http://discord",
-    shortUrl: "link.nakres.dev/",
-    shortenedDate: "2024-09-16",
-    shortenedTime: "00:58:47",
-  },
-  {
-    tag: "discord",
-    clickCount: 20,
-    originalUrl: "http://discord",
-    shortUrl: "link.nakres.dev/",
-    shortenedDate: "2024-09-16",
-    shortenedTime: "00:58:47",
-  },
-  {
-    tag: "discord",
-    clickCount: 20,
-    originalUrl: "http://discord",
-    shortUrl: "link.nakres.dev/",
-    shortenedDate: "2024-09-16",
-    shortenedTime: "00:58:47",
-  },
-];
-
-function LinksPreview() {
+function LinksPreview({ Links }: { Links: LinkType[] | null }) {
   return (
     <div className="bg-white p-3 rounded-lg shadow-cover space-y-3 max-h-screen overflow-auto">
-      {shortLinkData.map((link, i) => (
+      {Links?.map((link, i) => (
         <LinkCard key={i} link={link} />
       ))}
     </div>
