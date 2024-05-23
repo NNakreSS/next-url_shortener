da5bbaeeb5f00b4bbad1fe43c15737395dbf6570
NNakreSS <serkanatmaca15@gmail.com>
2024-05-23 20:47:46 +0300

    get links data from db

--- src/app/(pages)/(panel)/dashboard/page.tsx
+++ src/app/(pages)/(panel)/dashboard/page.tsx
@@ -1,13 +1,31 @@
+import { getCurrentUser } from "@/app/actions/getCurrentUser";
+import getUserClicks from "@/app/actions/getUserClicks";
+import getUserLinks from "@/app/actions/getUserLinks";
 import LastClickCard from "@/app/components/dashboard/main/LastClickCard";
 import LinkManagment from "@/app/components/dashboard/main/LinkManagment";
 import TotalCards from "@/app/components/dashboard/main/TotalCards";
 
-export default function Dashboard() {
+export default async function Dashboard() {
+  const currentUser = await getCurrentUser();
+  if (!currentUser) {
+    return (
+      <div>
+        <span className="text-red-500">
+          Bir sorun oluştu , Kullanıcı verilerine erişilemiyor
+        </span>
+      </div>
+    );
+  }
+
+  const Links = await getUserLinks(currentUser?.id);
+  const Clicks = await getUserClicks(currentUser?.id);
+
+  console.log(Clicks);
   return (
     <>
       <TotalCards />
       <div className="grid grid-cols-3 gap-5 mb-5">
-        <LinkManagment className="col-span-full xl:col-span-2" />
+        <LinkManagment Links={Links} className="col-span-full xl:col-span-2" />
         <div className="col-span-full xl:col-span-1">
           <LastClickCard />
         </div>
