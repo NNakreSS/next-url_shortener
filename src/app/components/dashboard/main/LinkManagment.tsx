ad5c4d88531b89d2b9624fddac82a75b6911b962
NNakreSS <serkanatmaca15@gmail.com>
2024-05-23 20:49:02 +0300

    prop drilling

--- src/app/components/dashboard/main/LinkManagment.tsx
+++ src/app/components/dashboard/main/LinkManagment.tsx
@@ -2,16 +2,18 @@ import { clsx } from "clsx";
 // components
 import Shortener from "../../general/Shortener";
 import LinksPreview from "./LinksPreview";
+import LinkType from "@/types/LinkType";
 
 interface LinkManagmentProps {
   className?: string;
+  Links: LinkType[] | null;
 }
 
-function LinkManagment({ className }: LinkManagmentProps) {
+async function LinkManagment({ className, Links }: LinkManagmentProps) {
   return (
     <section className={clsx(className, "flex flex-col gap-5")}>
       <Shortener />
-      <LinksPreview />
+      <LinksPreview Links={Links} />
     </section>
   );
 }
