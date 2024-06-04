a81eaaefe418a1c2375de5092f9424859c6b3195
NNakreSS <serkanatmaca15@gmail.com>
2024-06-04 17:16:26 +0300

    add toastContainer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { kanit } from "@/app/fonts";
 import "./styles/globals.css";
 import clsx from "clsx";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export const metadata: Metadata = {
   title: "nkLink",
@@ -15,7 +17,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={clsx(kanit.className, "bg-background")}>{children}</body>
+      <body className={clsx(kanit.className, "bg-background")}>
+        {children}
+        <ToastContainer />
+      </body>
     </html>
   );
 }
