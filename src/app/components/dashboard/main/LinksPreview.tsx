c91993ea860c8790309743b210d653108701c4f5
NNakreSS <serkanatmaca15@gmail.com>
2024-06-04 17:17:03 +0300

    refactor

--- src/app/components/dashboard/main/LinksPreview.tsx
+++ src/app/components/dashboard/main/LinksPreview.tsx
@@ -1,12 +1,16 @@
-"use client";
 import LinkCard from "./LinkCard";
 import LinkType from "@/types/LinkType";
 
-function LinksPreview({ Links }: { Links: LinkType[] | null }) {
+interface props {
+  Links: LinkType[] | null;
+  setLinks?: (prev?: any) => void;
+}
+
+function LinksPreview({ Links, setLinks }: props) {
   return (
     <div className="bg-white p-3 rounded-lg shadow-cover space-y-3 max-h-screen overflow-auto">
       {Links?.map((link, i) => (
-        <LinkCard key={i} link={link} />
+        <LinkCard key={i} link={link} setLinks={setLinks} />
       ))}
     </div>
   );
